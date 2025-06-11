8f8ef84c938c9bab2316c8a6c6b95a5a6bbdc7f6
yupig923 <154400859+yupig923@users.noreply.github.com>
2025-06-11 18:40:58 +0200

    reversed waterhead plot

--- postprocessing.m
+++ postprocessing.m
@@ -17,61 +17,103 @@ PfromExtractionData = getData(PfromExtraction);
 DStorageData = getData(DStorage);
 h_outData = getData(h_energy);
 
+
 %% Supply and Demand
+idx_filtered = find(mod(tout, 86400) == 0);  % find index for the first second in a week
+time_weeks = tout(idx_filtered) / unit("week");
+samples_per_week = 86400 / mean(diff(tout)); 
+n_weeks = floor(length(tout) / samples_per_week);
+
+weekly_sums_supply = [PSupplyData(1); zeros(n_weeks, 1)];
+for i = 1:n_weeks
+    idx_start = round((i - 1) * samples_per_week) + 1;
+    idx_end = round(i * samples_per_week);
+    weekly_sums_supply(i) = sum(PSupplyData(idx_start:idx_end));
+end
+weekly_sums_demand = [PDemandData(1); zeros(n_weeks, 1)];
+for i = 1:n_weeks
+    idx_start = round((i - 1) * samples_per_week) + 1;
+    idx_end = round(i * samples_per_week);
+    weekly_sums_demand(i) = sum(PDemandData(idx_start:idx_end));
+end
 fig = figure;
-plot(tout/unit("week"), PSupplyData/unit("kW"));
+plot(time_weeks, weekly_sums_supply/unit("W"));
 hold on;
-plot(tout/unit("week"), PDemandData/unit("kW"));
-xlim([0 tout(end)/unit("week")]);
+plot(time_weeks, weekly_sums_demand/unit("W"));
+xlim([0 time_weeks(end)]);
 grid on;
-title('Supply and Demand');
+title('Supply and Demand ');
 xlabel('Time [week]');
-ylabel('Power [kW]');
+ylabel('Power [W]');
 legend("Supply","Demand");
+set(gcf, 'Position', [100, 100, 1000, 400]);
 saveas(fig, 'fig_supply_demand.png');
 
 %% Stored Energy
 fig = figure;
-plot(tout/unit("week"), EStorageData/unit("J"));
+plot(time_weeks, EStorageData(idx_filtered)/unit("J"));
 xlim([0 tout(end)/unit("week")]);
 grid on;
 title('Stored Energy');
 xlabel('Time [week]');
 ylabel('Energy [J]');
+set(gcf, 'Position', [100, 100, 1000, 400]);
 saveas(fig, 'fig_stored_energy.png');
 
 %% Energy Losses
+weekly_sums_loss = [DData(1); zeros(n_weeks, 1)];
+for i = 1:n_weeks
+    idx_start = round((i - 1) * samples_per_week) + 1;
+    idx_end = round(i * samples_per_week);
+    weekly_sums_loss(i) = sum(DData(idx_start:idx_end));
+end
 fig = figure;
-plot(tout/unit("week"), DData/unit("W"));
+plot(time_weeks, weekly_sums_loss/unit("W"));   
 xlim([0 tout(end)/unit("week")]);
 grid on;
 title('Energy Losses');
 xlabel('Time [week]');
 ylabel('Dissipation Rate [W]');
+set(gcf, 'Position', [100, 100, 1000, 400]);
 saveas(fig, 'fig_energy_losses.png');
 
 %% Load Balancing
+weekly_sums_sell = [PSellData(1); zeros(n_weeks, 1)];
+for i = 1:n_weeks
+    idx_start = round((i - 1) * samples_per_week) + 1;
+    idx_end = round(i * samples_per_week);
+    weekly_sums_sell(i) = sum(PSellData(idx_start:idx_end));
+end
+weekly_sums_buy = [PBuyData(1); zeros(n_weeks, 1)];
+for i = 1:n_weeks
+    idx_start = round((i - 1) * samples_per_week) + 1;
+    idx_end = round(i * samples_per_week);
+    weekly_sums_buy(i) = sum(PBuyData(idx_start:idx_end));
+end
 fig = figure;
-plot(tout/unit("week"), PSellData/unit("W"));
+plot(time_weeks, weekly_sums_sell/unit("W"));
 hold on;
-plot(tout/unit("week"), PBuyData/unit("W"));
+plot(time_weeks, weekly_sums_buy/unit("W"));
 xlim([0 tout(end)/unit("week")]);
 grid on;
 title('Load Balancing');
 xlabel('Time [week]');
 ylabel('Power [W]');
 legend("Sell","Buy");
+set(gcf, 'Position', [100, 100, 1000, 400]);
 saveas(fig, 'fig_load_balancing.png');
 
 %% Tank Water Height
-if exist('h_outData', 'var') && all(isfinite(h_outData)) && any(h_outData <= 0)
+if exist('h_outData', 'var') && all(isfinite(h_outData)) && any(h_outData >= 0)
     fig = figure;
-    plot(tout/unit("week"), h_outData, 'b');
+    plot(time_weeks, h_outData(idx_filtered), 'b');
     xlim([0 tout(end)/unit("week")]);
     grid on;
     title('Water Height in Tanks');
     xlabel('Time [week]');
     ylabel('Height [m]');
+    set(gca, 'YDir', 'reverse')
+    set(gcf, 'Position', [100, 100, 1000, 400]);
     saveas(fig, 'fig_water_height.png');
 else
     warning("Tank water height (h_out) is missing or invalid. Skipping plot.");
